d669d4360893f8307f5e76947543db672a933a7f
Lucas Mendes <90008972+Luquinhazm@users.noreply.github.com>
2022-05-08 00:59:28 -0300

    modal

---
+++ modal.js
@@ -0,0 +1,19 @@
+console.log('hello word')
+
+const btnModal = document.getElementById('btnRules')
+const modalRunes = document.getElementById('modalRules')
+const btnClose = document.getElementById('btn--close')
+
+
+function switchModal(){
+    const actualStlye = modalRunes.style.display
+    if(actualStlye === 'flex'){
+        modalRunes.style.display = 'none'
+    } else{
+        modalRunes.style.display = 'flex'
+    }
+}
+
+btnModal.addEventListener('click', switchModal)
+btnClose.addEventListener('click', switchModal)
+

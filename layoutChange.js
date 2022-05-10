9067751f71fc15284309f2211239ecc5e2a25e61
Lucas Mendes <90008972+Luquinhazm@users.noreply.github.com>
2022-05-10 00:19:17 -0300

    logica completa

--- layoutChange.js
+++ layoutChange.js
@@ -4,6 +4,7 @@ const btnScissors = document.getElementById('btnScissors')
 
 const esconder = document.getElementById('hidden__mainPage')
 const escolhaPage = document.getElementById('choosePage')
+const textResult = document.getElementById('textResult')
 
 const rock = document.getElementById('pickedRock')
 const paper = document.getElementById('pickedPaper')
@@ -14,11 +15,15 @@ const scissorsPc = document.getElementById('pickedScissorsPc')
 const result = document.getElementById('result')
 const btnPlayAgain = document.getElementById('playAgain')
 
+const score = document.getElementById('scoreRecord')
+let scoreContent = 0
+let choosePlayer = ''
 
 btnPaper.addEventListener('click', ()=>{
     esconder.classList.add('main__hidden')
     escolhaPage.style.display = 'flex'
     paper.style.display = 'flex'
+    choosePlayer = paper
     jokenpo()
 } )
 
@@ -26,6 +31,7 @@ btnRock.addEventListener('click', ()=>{
     esconder.classList.add('main__hidden')
     escolhaPage.style.display = 'flex'
     rock.style.display = 'flex'
+    choosePlayer = rock
     jokenpo()
 } )
 
@@ -33,29 +39,62 @@ btnScissors.addEventListener('click', ()=>{
     esconder.classList.add('main__hidden')
     escolhaPage.style.display = 'flex'
     scissors.style.display = 'flex'
+    choosePlayer = scissors
     jokenpo()
 } )
 
 function jokenpo() {
     const escolhaPC = Math.floor(Math.random() * 3)
+    let choosePc = ''
     
     switch(escolhaPC) {
         case 0 :
             paperPc.style.display = 'flex'
-            
+            choosePc = paper
         break
         case 1 :
              scissorsPc.style.display = 'flex'
-
+            choosePc = scissors
          break
         case 2 :
              rockPc.style.display = 'flex'
-                    
+            choosePc = rock
          break
                     }
-                
+        
+        if(choosePlayer === paper && choosePc === rock){
+            textResult.textContent = 'win'
+           incrementar()
+        }else if(choosePlayer === paper && choosePc === scissors){
+            textResult.textContent = 'defeat'
+            decrementar()
+        }else if(choosePlayer === paper && choosePc === paper){
+            textResult.textContent = 'draw'
+        }
+
+         
+        if(choosePlayer === scissors && choosePc === paper){
+            textResult.textContent = 'win'
+            incrementar()
+        }else if(choosePlayer === scissors && choosePc === rock){
+            textResult.textContent = 'defeat'
+            decrementar()
+        }else if(choosePlayer === scissors && choosePc === scissors){
+            textResult.textContent = 'draw'
+        }
+
+        if(choosePlayer === rock && choosePc === scissors){
+            textResult.textContent = 'win'
+            incrementar()
+        }else if(choosePlayer === rock && choosePc === paper){
+            textResult.textContent = 'defeat'
+            decrementar()
+        }else if(choosePlayer === rock && choosePc === rock){
+            textResult.textContent = 'draw'
+        }
+
+
         result.style.display = 'block'
-            
 }
 
 
@@ -71,3 +110,15 @@ function clearScreen(){
 }
 
 btnPlayAgain.addEventListener('click', clearScreen)
+
+
+function decrementar(){
+    for(scoreContent ; scoreContent >= 1 ; --scoreContent){
+        score.textContent = "0"
+    }
+}
+
+function incrementar(){
+    ++scoreContent
+    score.textContent = scoreContent
+}
